3977b3de34b74fdfd16f4020b4eafad2ae96a6a1
Eliyahu Shlieserman <eliyahushli@gmail.com>
2016-01-30 15:19:51 +0200

    ADD CREDIT FIX

---
+++ Credit/MasterCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Credit
+{
+    public class MasterCard : ICreditCard
+    {
+        public int ChargeCount { get; set; }
+
+        public string Charge()
+        {
+            ChargeCount++;
+            return "this is MasterCard";
+        }
+    }
+}
---
+++ Credit/Visa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit
+{
+    public class Visa : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Visa ... Visa";
+        }
+
+
+        public int ChargeCount
+        {
+            get { return 0; }
+        }
+    }
+}
